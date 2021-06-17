6ac3a647fe279b40dc13a2e3534dc9a399fc5355
Marijn Haverbeke <marijn@haverbeke.nl>
2021-06-17 18:13:55 +0200

    Port tokenizers to new InputStream interface

--- src/tokens.js
+++ src/tokens.js
@@ -1,5 +1,5 @@
 import {ExternalTokenizer} from "lezer"
-import {rawStringStart, rawStringEnd, rawStringContent, templateArgsEndFallback, MacroName} from "./parser.terms.js"
+import {RawString, templateArgsEndFallback, MacroName} from "./parser.terms.js"
 
 const R = 82, L = 76, u = 117, U = 85,
       a = 97, z = 122, A = 65, Z = 90, Underscore = 95,
@@ -9,69 +9,57 @@ const R = 82, L = 76, u = 117, U = 85,
       Space = 32, Newline = 10,
       GreaterThan = 62
 
-export const rawString = new ExternalTokenizer((input, token) => {
-  let pos = token.start, next = input.get(pos++)
+export const rawString = new ExternalTokenizer(input => {
   // Raw string literals can start with: R, LR, uR, UR, u8R
-  if (next == L || next == U) {
-    next = input.get(pos++)
-  } else if (next == u) {
-    next = input.get(pos++)
-    if (next == Zero + 8) next = input.get(pos++)
+  if (input.next == L || input.next == U) {
+    input.advance()
+  } else if (input.next == u) {
+    input.advance()
+    if (input.next == Zero + 8) input.advance()
   }
-  if (next != R) return
-  next = input.get(pos++)
-  if (next != Quote) return
-  next = input.get(pos++)
+  if (input.next != R) return
+  input.advance()
+  if (input.next != Quote) return
+  input.advance()
 
-  while (next != ParenL) {
-    if (next == Space || next <= 13 || next == ParenR) return
-    next = input.get(pos++)
+  let marker = ""
+  while (input.next != ParenL) {
+    if (input.next == Space || input.next <= 13 || input.next == ParenR) return
+    marker += String.fromCharCode(input.next)
+    input.advance()
   }
+  input.advance()
 
-  return token.accept(rawStringStart, pos)
+  for (;;) {
+    if (input.next < 0)
+      return input.acceptToken(RawString)
+    if (input.next == ParenR) {
+      let match = true
+      for (let i = 0; match && i < marker.length; i++)
+        if (input.peek(i + 1) != marker.charCodeAt(i)) match = false
+      if (match && input.peek(marker.length + 1) == Quote)
+        return input.acceptToken(RawString, 2 + marker.length)
+    }
+    input.advance()
+  }
 })
 
-export const rawStringContinue = new ExternalTokenizer((input, token, stack) => {
-  let pos = token.start, next = input.get(pos++), markerString = null
-  for (;;) {
-    if (next < 0) {
-      if (pos > token.start + 1) token.accept(rawStringContent, pos - 1)
-      return
-    } else if (next == ParenR) {
-      if (!markerString) {
-        let ruleStart = stack.ruleStart
-        let match = /"(\S*?)\(/.exec(input.read(ruleStart, Math.min(token.start, ruleStart + 100)))
-        if (!match) return
-        markerString = match[1] + '"'
-      }
-      if (input.read(pos, pos + markerString.length) == markerString) {
-        token.accept(rawStringEnd, pos + markerString.length)
-        return
-      }
-    } else if (next == Newline) {
-      token.accept(rawStringContent, pos)
-      return
-    }
-    next = input.get(pos++)
-  }
-}, {contextual: true})
-
-export const fallback = new ExternalTokenizer((input, token) => {
-  let pos = token.start, next = input.get(pos)
-  if (next == GreaterThan) {
+export const fallback = new ExternalTokenizer(input => {
+  if (input.next == GreaterThan) {
     // Provide a template-args-closing token when the next characters
     // are ">>", in which case the regular tokenizer will only see a
     // bit shift op.
-    if (input.get(pos + 1) == GreaterThan)
-      token.accept(templateArgsEndFallback, pos + 1)
+    if (input.peek(1) == GreaterThan)
+      input.acceptToken(templateArgsEndFallback, 1)
   } else {
     // Notice all-uppercase identifiers
-    let sawLetter = false
-    for (;; next = input.get(++pos)) {
-      if (next >= A && next <= Z) sawLetter = true
-      else if (next >= a && next <= z) return
-      else if (next != Underscore && !(next >= Zero && next <= Zero + 9)) break
+    let sawLetter = false, i = 0
+    for (;; i++) {
+      if (input.next >= A && input.next <= Z) sawLetter = true
+      else if (input.next >= a && input.next <= z) return
+      else if (input.next != Underscore && !(input.next >= Zero && input.next <= Zero + 9)) break
+      input.advance()
     }
-    if (sawLetter && pos >= token.start + 2) token.accept(MacroName, pos)
+    if (sawLetter && i > 1) input.acceptToken(MacroName)
   }
 }, {extend: true})
